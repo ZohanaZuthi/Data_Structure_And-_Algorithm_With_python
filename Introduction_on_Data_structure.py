aa139b61a3661a188769b23dd31ac6244e3a9f3e
Nazifa Fairuz Zuthi <startrack613@gmail.com>
2024-09-02 05:37:22 +0600

    Create Introduction_on_Data_structure.py

---
+++ Introduction_on_Data_structure.py
@@ -0,0 +1,3 @@
+# A data structure is a particular way of organizing data in a computer's memory so that it can be used effectively by program code.
+# if data is stored in hard disk that is called DBMS
+# the part of computer that does computation is called CPU which is mainly proccessor.
